73197d269ff36c20623655a534e27ca714b1c1c0
Jens De Temmerman <jens@jensdt.be>
2018-01-04 13:32:54 +0100

    Made postdeploy optional; depending on existence of CF variables

--- postdeploy.js
+++ postdeploy.js
@@ -1,40 +1,50 @@
 const AWS = require('aws-sdk');
-AWS.config.update({region: 'eu-west-1', credentials:
-    new AWS.Credentials(process.env.CF_AWS_ACCESS_KEY_ID, process.env.CF_AWS_SECRET_ACCESS_KEY)
-});
 
-const cf = new AWS.CloudFormation();
-const herokuAppName = process.env.HEROKU_APP_NAME;
-const baseQueueName = process.env.SQS_QUEUE_NAME;
-const queueName = `${baseQueueName}-${herokuAppName}`;
+if (!process.env.CF_AWS_ACCESS_KEY_ID || !process.env.CF_AWS_SECRET_ACCESS_KEY) {
+    console.log('CF_AWS_ACCESS_KEY_ID or CF_AWS_SECRET_ACCESS_KEY not set, not running postdeploy script');
+} else {
+    createStack();
+}
 
-const template = {
-    Resources: {
-        "MyAutoCreatedQueue": {
-            Type: "AWS::SQS::Queue",
-            Properties: {
-                QueueName: queueName
+
+function createStack() {
+    AWS.config.update({region: 'eu-west-1', credentials:
+        new AWS.Credentials(process.env.CF_AWS_ACCESS_KEY_ID, process.env.CF_AWS_SECRET_ACCESS_KEY)
+    });
+
+    const cf = new AWS.CloudFormation();
+    const herokuAppName = process.env.HEROKU_APP_NAME;
+    const baseQueueName = process.env.SQS_QUEUE_NAME;
+    const queueName = `${baseQueueName}-${herokuAppName}`;
+
+    const template = {
+        Resources: {
+            "MyAutoCreatedQueue": {
+                Type: "AWS::SQS::Queue",
+                Properties: {
+                    QueueName: queueName
+                }
             }
         }
-    }
-};
+    };
 
-cf.createStack({
-    StackName: herokuAppName,
-    TemplateBody: JSON.stringify(template)
-}, function (err) {
-    if (err) {
-        console.error(err, err.stack);
-        throw new Error('Stack creation failed');
-    } else {
-        cf.waitFor('stackCreateComplete', {
-            StackName: herokuAppName
-        }, function (err) {
-            if (err) {
-                console.error(err, err.stack);
-                throw new Error('Failed to wait for stack completion');
-            }
-        });
+    cf.createStack({
+        StackName: herokuAppName,
+        TemplateBody: JSON.stringify(template)
+    }, function (err) {
+        if (err) {
+            console.error(err, err.stack);
+            throw new Error('Stack creation failed');
+        } else {
+            cf.waitFor('stackCreateComplete', {
+                StackName: herokuAppName
+            }, function (err) {
+                if (err) {
+                    console.error(err, err.stack);
+                    throw new Error('Failed to wait for stack completion');
+                }
+            });
 
-    }
-});
+        }
+    });
+}
